3b32417b95ca1d3b192b367e37a01261bf080d98
G0WTHAM0fficial <gowtamp2003@gmail.com>
2025-05-12 14:12:13 +0530

    update 1

--- frontend/Landpg/vite.config.js
+++ frontend/Landpg/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // important if you're refreshing pages in development
-    historyApiFallback: true
-  }
+  base:'/Solvstrat',
 })
